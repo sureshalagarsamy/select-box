229624d51453b986a27756e50afa8dd1c92695ea
sureshalagarsamy <asuresh@zoho.com>
2016-10-17 17:58:03 +0530

    Deleting this file

--- myCrtl.js
+++
@@ -1,7 +0,0 @@
-function myCrtl($scope)
-{   
-  $scope.myObj = {};
-  $scope.myObj.price = 20;
-  $scope.myObj.items = [{name: 'one', age: 30 },{ name: 'two', age: 27 },{ name: 'three', age: 50 }];
-  $scope.selectedData = $scope.myObj.items[0];
-}
